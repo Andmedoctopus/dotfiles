94ed58e89219dc5624ee83672bfae89976fb577a
Andmedoctopus <andmed999@gmail.com>
2020-01-27 22:20:19 +0300

    Upload config from .dotfiles to system

--- installer.sh
+++ installer.sh
@@ -3,17 +3,9 @@ sudo apt-get update
 sudo apt-get install -y neovim python3-pip python3-setuptools libreadline-gplv2-dev libncursesw5-dev libssl-dev /
 libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libinput-tools tmux
 pyhthon3 -m pip --upgrade pynvim
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo chsh -s $(which zsh)
-mkdir ~/.config/nvim
-cp init.vim ~/.config/nvim
-cp .zshrc ~/.
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim -c "PlugInstall" -c "!q"
-git config --global user.name "Andmedoctopus" 
-git config --global user.email "andmed999@gmail.com"
 
 cd
 git clone https://github.com/gpakosz/.tmux.git
@@ -27,3 +19,17 @@ sudo make install
 git clone https://gitlab.com/cunidev/gestures
 cd gestures
 sudo python3 setup.py install
+
+mkdir ~/.config/nvim
+cp init.vim ~/.config/nvim
+
+cp .zshrc ~/.
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp .tmux.conf.local ~/.
+
+nvim -c "PlugInstall" -c "!q"
+
+
