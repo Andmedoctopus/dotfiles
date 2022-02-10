e8dec15dea8154048ae57354d2df671396111ec5
Andmedoctopus <andmed999@gmail.com>
2022-02-10 21:48:10 +0300

    Add install make for installtion

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible git
+sudo apt install -y ansible git make
 STORE_PATH="/home/$USER/.config/.dotfiles"
 
 git clone https://github.com/Andmedoctopus/dotfiles $STORE_PATH
