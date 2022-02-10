02fef002825b2f59d39d35c5cc9d022965d155f7
Andmedoctopus <andmed999@gmail.com>
2022-02-10 20:58:36 +0300

    Add install command to Makefile

--- install.sh
+++ install.sh
@@ -3,15 +3,10 @@
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible git
 STORE_PATH="/home/$USER/.config/.dotfiles"
-SETUP_FILE="ansible/setup_dev_env.yaml"
 
 git clone https://github.com/Andmedoctopus/dotfiles $STORE_PATH
 cd $STORE_PATH
 # to remove when merge to master
 git checkout ansible-dev
 
-read -p "Do you wish to install this program?" yn
-case $yn in
-    [Yy]* ) ansible-playbook $SETUP_FILE --ask-become-pass; break;;
-    * ) echo Visit $STORE_PATH/$SETUP_FILE to configure; exit;;
-esac
+make install
