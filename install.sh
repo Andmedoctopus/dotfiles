99efc0502aad1939a27152c59db7111fc4169008
Andmedoctopus <andmed999@gmail.com>
2022-02-10 17:22:09 +0300

    Fix git clone typo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt install -y ansible git
 STORE_PATH="~/.config/.dotfiles"
 SETUP_FILE="setup_dev_env.yaml"
 
-git pull https://github.com/Andmedoctopus/dotfiles $STORE_PATH
+git clone https://github.com/Andmedoctopus/dotfiles $STORE_PATH
 cd $STORE_PATH
 # to remove when merge to master
 git checkout ansible-dev
