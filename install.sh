fba0192175bf3ebbdacb4e47de00abe0d2075513
Andmedoctopus <andmed999@gmail.com>
2022-02-18 16:24:00 +0300

    Add prompt for installation

--- install.sh
+++ install.sh
@@ -7,4 +7,12 @@ STORE_PATH="/home/$USER/.config/.dotfiles"
 git clone https://github.com/Andmedoctopus/dotfiles $STORE_PATH
 cd $STORE_PATH
 
-make install
+while true; do
+    read -p "Do you wish to install this program?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
